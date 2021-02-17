6b93dc7a1b91d89aee9c37b2cc224e098ccee2c6
Brian Carcich <briantcarcich@gmail.com>
2021-02-17 15:37:13 -0500

    Integrate PID into chilled tank model

--- pid.py
+++ pid.py
@@ -16,22 +16,23 @@ windup beyond [CVlo:CVhi]
   """
   def __init__(self
               ,Kc,Ti,Td             ### Ratio, Minutes, Minutes
-              ,Updatetime           ### Seconds
-              ,PVlo=0.0,PVhi=100.0  ### See comment in self.__doc__
-              ,CVlo=0.0,CVhi=100.0 
-              ,CVlast=50.0
+              ,CVlast=50.0          ### % of CV range
+              ,Updatetime=45.0      ### Seconds
+              ,Deadband=0.01        ### % of PV range (not yet implemented)
               ,Direct=True          ### E(rror) = PV - SP; CV will increase in response to increasing PV
                                     ### Direct=False => E(rror) = SP - PV;
               ,Auto=True
+              ,PVlo=0.0,PVhi=100.0  ### See comment in self.__doc__
+              ,CVlo=0.0,CVhi=100.0 
               ):
     """Store inputs"""
 
     (self.Kc,self.Ti,self.Td
-    ,self.Updatetime
-    ,self.CVlast,self.Direct,self.Auto
+    ,self.CVlast,self.Updatetime,self.Deadband
+    ,self.Direct,self.Auto
     ,) = (Kc,Ti,Td
-         ,Updatetime
-         ,CVlast,PVs,Direct,Auto
+         ,CVlast,Updatetime,Deadband
+         ,Direct,Auto
          ,)
 
     ### Initialize empty past PVs list for Td, last error for Kc, for
@@ -39,15 +40,15 @@ windup beyond [CVlo:CVhi]
     self.lastPVs,self.lastError = [],None 
 
     ### Setup scaling and limits
-    self.PVlims(PVlo,PVhi)
-    self.CVlims(CVlo,CVhi)
+    self.set_PVlims(PVlo,PVhi)
+    self.set_CVlims(CVlo,CVhi)
 
 
   def control(self,PVexternal,SPexternal):
     """Execute one update of control algorithm"""
 
     ### Ingest external Present (measured) Value and SetPoint
-    PVinternal,SPinternal = map(self.ingestPV,(PV,SPexternal,))
+    PVinternal,SPinternal = map(self.ingestPV,(PVexternal,SPexternal,))
 
     ### Calculate current error
     if self.Direct: err = PVinternal - SPinternal
@@ -68,19 +69,28 @@ windup beyond [CVlo:CVhi]
                         )
 
     ### Save .lastPVs and last error
-    self.lastPVs,self.lastError = [self.lastPVs[-1],PVinternal],error
+    self.lastPVs,self.lastError = [self.lastPVs[-1],PVinternal],err
 
     ### Calculate new CV, save as .CVlast
-    self.CVlast =  self.emitCV(deltaCVinternal)
+    self.CVlast =  self.emitCV(deltaCV)
     return self.CVlast
 
   def ingestPV(self,PVexternal):
+    """
+    Ingest PV from extenal units to internal units, clamp as needed
+
+    """
     if   PVexternal > self.PVmax: clampPV = self.PVmax
     elif PVexternal < self.PVmin: clampPV = self.PVmin
     else                        : clampPV = PVexternal
     return (clampPV-self.PVlo) / self.PVmag
 
-  def emitCV(self,deltaCVinternal):
+  def emitCV(self,deltaCV):
+    """
+    Scale internal change in CV to external units,
+    add to last CV, scale as needed
+
+    """
     CVexternal =  self.CVlast + (deltaCV * self.CVmag)
     if CVexternal > self.CVmax: return self.CVmax
     if CVexternal < self.CVmin: return self.CVmin
--- pv_predict.py
+++ pv_predict.py
@@ -1,11 +1,14 @@
 import os
 import sys
+import pid
+import math
 import numpy as np
 import traceback as tb
 import read_interleaved_XLSX as riX
 
 do_debug = 'DEBUG' in os.environ
 do_warn = 'WARN' in os.environ
+npzs = lambda L: np.zeros(L,dtype=np.float)
 
 class CET:  ### Chilled Exothermic Tank
   """
@@ -37,7 +40,18 @@ Model
   default_time_step = 1.00    ### Time step, seconds
   default_init_temp = 11.87   ### Temperature to look for to start model
   default_init_CV = 0.0       ### CV to look for to start model
+
+  ### Source data to which to compare model
   default_pathv = ('Tank_20_Results_Feb_11-12_2021_R1.xlsx',)
+
+  ### Default PID parameters (poor control)
+  default_pid_Kc = 5.0           ### Gain ratio
+  default_pid_Ti = 8.0           ### minutes
+  default_pid_Td = 1.5           ### minutes
+  default_pid_updatetime = 45.0  ### seconds
+  default_pid_deadband = 0.01    ### % of PV range
+  default_pid_setpoint = 12.00   ### degrees Celsius
+  default_pid_duration = 86400.  ### seconds
 
   def __init__(self,*args,**keywords):
     """Initialize model parameters, read model data"""
@@ -50,8 +64,16 @@ Model
     self.init_temp = float(keywords.get('init-temp',self.default_init_temp))
     self.init_CV = float(keywords.get('init-CV',self.default_init_CV))
     self.kPV = float(keywords.get('kPV',self.default_kPV))
-    self.time_step = float(keywords.get('time-step',self.default_time_step))
+    self.model_time_step = float(keywords.get('model-time-step',self.default_time_step))
     self.do_plot = not keywords.get('no-plot',False)
+
+    self.pid_Kc = float(keywords.get('pid-Kc',self.default_pid_Kc))
+    self.pid_Ti = float(keywords.get('pid-Ti',self.default_pid_Ti))
+    self.pid_Td = float(keywords.get('pid-Td',self.default_pid_Td))
+    self.pid_updatetime = float(keywords.get('pid-updatetime',self.default_pid_updatetime))
+    self.pid_deadband = float(keywords.get('pid-deadband',self.default_pid_Kc))
+    self.pid_setpoint = float(keywords.get('pid-setpoint',self.default_pid_setpoint))
+    self.pid_duration = float(keywords.get('pid-duration',self.default_pid_duration))
 
     ### Model data from XLSX
     self.path = None
@@ -72,20 +94,20 @@ Model
     ### Convert model parameter inputs to per-timestep values
     ### - .Ke - temperature rise per timestep, exotherm-only, no cooling
     ### - .kPV_step - per-timestep PV decay, (PVn-Tt)/(PV<n-1>-Tt)
-    self.Ke = self.Ke_per_h * self.time_step / 3600.0
-    self.kPV_step = self.kPV**self.time_step
+    self.Ke = self.Ke_per_h * self.model_time_step / 3600.0
+    self.kPV_step = self.kPV**self.model_time_step
 
   def model_one_timestep(self,AT,Tt,PV,CVscalar):
     retTt = Tt + CVscalar
     retPV = retTt + ((PV - retTt) * self.kPV_step) + self.Ke
-    retAT = AT + self.time_step
+    retAT = AT + self.model_time_step
     return retAT,retTt,retPV
 
   def calculate_CVscalar(self,CV):
     if CV > self.CVe0: return self.Ke * (1.0 - ((CV - self.CVe0) / (self.CVe - self.CVe0))**self.CVexponent)
     return self.Ke
 
-  def model_data(self,do_plot=None,short_circuit=False):
+  def model_data(self,do_plot=None):
 
     ### Select start point from raw data
     i0 = np.where(np.bitwise_and(self.cvs==self.init_CV,self.pvs==self.init_temp))[0][0]-1
@@ -95,7 +117,7 @@ Model
 
     ### Create predicted data arrays
     L = len(pATs)
-    pPVs,pTts = np.zeros(L),np.zeros(L)
+    pPVs,pTts = npzs(L),npzs(L)
 
     ### Get Present Value from model
     pPV = self.pvs[i0:i0+2].mean()
@@ -111,7 +133,7 @@ Model
         pPVs[inext],pTts[inext] = pPV,pTt
         CVscalar = self.calculate_CVscalar(CV)
         inext += 1
-        if inext==L: break
+        if inext>=L: break
 
       AT,pTt,pPV = self.model_one_timestep(AT,pTt,pPV,CVscalar)
 
@@ -124,9 +146,9 @@ Model
               ,self.CVe
               ,self.CVexponent
               )
-      self.plot_data(pATs,None,pPVs,pTts,title)
+      self.plot_data(pATs,None,pPVs,pTts,title,cvtitle=None)
 
-  def plot_data(self,ATs,CVs,PVs,Tts,title):
+  def plot_data(self,ATs,CVs,PVs,Tts,title,cvtitle=None):
     import matplotlib.pyplot as plt
 
     fig,(pvplt,cvplt) = plt.subplots(nrows=2,ncols=1
@@ -135,7 +157,8 @@ Model
                                     )
 
     pvplt.axhline(12.0,label='SP',linewidth=0.5,linestyle='dotted')
-    pvplt.plot(self.ats,self.pvs,label='PV Data')
+    if None is CVs:
+      pvplt.plot(self.ats,self.pvs,label='PV Data')
     pvplt.plot(ATs,PVs,label='PV Predict')
     pvplt.plot(ATs,Tts,label='Tank Predict')
     pvplt.set_ylabel('Temperature, degC')
@@ -148,11 +171,56 @@ Model
       cvplt.plot(ATs,CVs,linewidth=0.5)
     cvplt.set_ylabel('CV, %')
     cvplt.set_xlabel('Time, s')
+    if isinstance(cvtitle,str): cvplt.set_title(cvtitle)
 
     plt.show()
 
-  def model_with_pid(self,pid):
-    pass
+  def model_with_pid(self,keywords,do_plot=None):
+    L = int(math.ceil(self.pid_duration / self.pid_updatetime))
+    ATs,rPVs,xTts,rCVs = npzs(L),npzs(L),npzs(L),npzs(L)
+    AT,xPV,xTt,xCV = 0.0,11.80,11.80,0.0
+
+    nextPIDAT,inext,lastrCV = 0.0,0,-1e32
+
+    ctlpid = pid.PID(self.pid_Kc,self.pid_Ti,self.pid_Td
+                    ,CVlast=xCV
+                    ,Updatetime=self.pid_updatetime
+                    ,Deadband=self.pid_deadband
+                    )
+
+    while True:
+      rPV = round(xPV,2)
+
+      if AT >= nextPIDAT:
+        xCV = ctlpid.control(rPV,self.pid_setpoint)
+        rCV = round(xCV,0)
+        if rCV != lastrCV: CVscalar = self.calculate_CVscalar(rCV)
+
+        ATs[inext],rPVs[inext],xTts[inext],rCVs[inext] = AT,rPV,xTt,rCV
+        inext += 1
+        if inext >= L: break
+        nextPIDAT = AT + self.pid_updatetime
+
+      AT,xTt,xPV = self.model_one_timestep(AT,xTt,xPV,CVscalar)
+
+    if self.do_plot if (None is do_plot) else do_plot:
+      title = '{0}\nKe={1}deg/h kPV={2} CVe0={3} CVe={4} CVexp={5}'.format(
+              os.path.basename(self.path)
+              ,self.Ke_per_h
+              ,self.kPV
+              ,self.CVe0
+              ,self.CVe
+              ,self.CVexponent
+              )
+      cvtitle = 'Kc={0} Ti={1}min Td={2}min Update={3}s'.format(
+                self.pid_Kc
+                ,self.pid_Ti
+                ,self.pid_Td
+                ,self.pid_updatetime
+                #,self.pid_deadband
+                )
+      self.plot_data(ATs,rCVs,rPVs,xTts,title,cvtitle=cvtitle)
+
 
 def process_args(argv):
   args,keywords = list(),dict()
@@ -168,4 +236,9 @@ def process_args(argv):
 
 if "__main__" == __name__:
   args,keywords = process_args(sys.argv[1:])
-  cet = CET(*args,**keywords).model_data()
+  print(keywords)
+  cet = CET(*args,**keywords)
+  if not ('no-model-data' in keywords):
+    cet.model_data()
+  if not ('no-model-pid' in keywords):
+    cet.model_with_pid(keywords)
